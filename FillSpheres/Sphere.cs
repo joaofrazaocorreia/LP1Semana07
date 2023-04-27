9933102f7588be45916803cab9e99ea344a71658
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-04-27 22:30:44 +0100

    Initialize variables and constructor for class Sphere

--- FillSpheres/Sphere.cs
+++ FillSpheres/Sphere.cs
@@ -7,6 +7,15 @@ namespace FillSpheres
 {
     public class Sphere
     {
-        
+        private Color color;
+        private int radius;
+        private int throwCount;
+
+        public Sphere(Color color, int radius, int throwCount)
+        {
+            this.color = color;
+            this.radius = radius;
+            throwCount = 0;
+        }
     }
 }
