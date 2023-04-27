6e0d8ce62c8f599351a630c664c3ff0426568e2e
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-04-27 23:10:32 +0100

    Throw and pop some spheres then print their states

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -6,7 +6,36 @@ namespace FillSpheres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Color white = new Color(255, 255, 255);
+            Color red = new Color(255, 0, 0);
+            Color yellow = new Color(255, 255, 0);
+            Color green = new Color(0, 255, 100);
+
+            Sphere[] mySpheres = new Sphere[4];
+
+            mySpheres[0] = new Sphere(red, 10);
+            mySpheres[1] = new Sphere(white, 5);
+            mySpheres[2] = new Sphere(green, 20);
+            mySpheres[3] = new Sphere(yellow, 7);
+
+            mySpheres[3].Throw();
+            mySpheres[0].Throw();
+            mySpheres[1].Pop();
+            mySpheres[1].Throw();
+            mySpheres[0].Pop();
+            mySpheres[3].Throw();
+            mySpheres[3].Throw();
+
+            for (int i = 0; i < mySpheres.Length; i++)
+            {
+                Console.WriteLine($"Sphere {i+1}'s state:");
+                Console.WriteLine($"Color: {mySpheres[i].GetColor()}");
+                Console.WriteLine($"Radius: {mySpheres[i].GetRadius()}");
+                Console.WriteLine($"Times thrown: " +
+                                    mySpheres[i].GetTimesThrown());
+                Console.WriteLine("");
+            }
+
         }
     }
 }
