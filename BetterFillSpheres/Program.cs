e3354df74894877935d408dba2b5df824bd315b5
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-04-27 23:32:10 +0100

    Create project BetterFillSpheres

---
+++ BetterFillSpheres/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BetterFillSpheres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
