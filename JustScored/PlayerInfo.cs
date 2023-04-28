1c82c47919b62b9e50ff489f02320508e0c67897
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-04-28 13:43:57 +0100

    Create and initialize class PlayerInfo

---
+++ JustScored/PlayerInfo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustScored
+{
+    public class PlayerInfo
+    {
+        private float maxScore;
+        private int gamesPlayed;
+        private int gamesWon;
+
+
+
+    }
+}
