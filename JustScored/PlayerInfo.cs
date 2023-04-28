c71e0cfd5a0ec058e2b8b6f22f6696feb59a605e
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-04-28 14:03:19 +0100

    Fix RateOfSuccess not calculating with a float

--- JustScored/PlayerInfo.cs
+++ JustScored/PlayerInfo.cs
@@ -39,7 +39,7 @@ namespace JustScored
 
                 else
                 {
-                    return gamesWon / gamesPlayed;
+                    return (float)gamesWon / gamesPlayed;
                 }
             }
         }
